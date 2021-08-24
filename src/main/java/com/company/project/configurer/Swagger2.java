eda7322c88873a8c0347275b7844314393281941
zhangchunsheng <zhangchunsheng423@gmail.com>
2021-08-24 12:49:27 +0800

    1. update mysql

--- src/main/java/com/company/project/configurer/Swagger2.java
+++ src/main/java/com/company/project/configurer/Swagger2.java
@@ -2,6 +2,8 @@ package com.company.project.configurer;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -10,7 +12,15 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
-public class Swagger2 {
+public class Swagger2 extends WebMvcConfigurerAdapter {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("index.html")
+                .addResourceLocations("classpath:/META-INF/webapp/");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
     /**
      * 创建API应用
      * apiInfo() 增加API相关信息
